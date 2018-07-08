377e38c23901c92978f52306022c9219454bfee2
Maxime Bret <xmax54@gmail.com>
2018-07-08 02:43:11 +0200

    standard js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-export { default as createLoader } from './createLoader';
-export { default as createMiddleware } from './createMiddleware';
-export { default as reducer } from './reducer';
-export { LOAD, SAVE } from './constants';
+export { default as createLoader } from './createLoader'
+export { default as createMiddleware } from './createMiddleware'
+export { default as reducer } from './reducer'
+export { LOAD, SAVE } from './constants'
 
 // The full default export is required to be BC with redux-storage <= v1.3.2
 export default {
@@ -9,4 +9,4 @@ export default {
     createLoader: require('./createLoader').default,
     createMiddleware: require('./createMiddleware').default,
     reducer: require('./reducer').default
-};
+}
